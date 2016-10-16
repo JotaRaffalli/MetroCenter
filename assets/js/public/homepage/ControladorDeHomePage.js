10a02d58876ec2907ba73556eeedae9ec714feb6
José Luis Raffalli <raffallijoseluis@gmail.com>
2016-10-15 22:34:10 -0400

    Mariqueras #4
    Ver cambios ... inutiles

--- assets/js/public/homepage/ControladorDeHomePage.js
+++ assets/js/public/homepage/ControladorDeHomePage.js
@@ -16,7 +16,7 @@ angular.module('ModuloHomePage').controller('ControladorDeHomePage', ['$scope', 
     })
     .then(function onSuccess (){
      // lleva a 
-      
+      window.location = '/';
       toastr["success"]("Bienvenido!");
 
     })
