3e4071d75b0e946be46e9522582e64b6f81e0398
José Luis Raffalli <raffallijoseluis@gmail.com>
2016-11-02 15:28:54 -0400

    Agregada Vista de Cubiculos
    Simple Tabla que muestra los Cubiculos disponibles

--- config/routes.js
+++ config/routes.js
@@ -38,6 +38,7 @@ module.exports.routes = {
   'POST /signup': 'UserController.create',
   'PUT /login'  : 'UserController.login',
   '/perfil' : 'PerfilController.perfil',
+  '/cubiculos' : {view: 'cubiculos'},
   
 
   /***************************************************************************
