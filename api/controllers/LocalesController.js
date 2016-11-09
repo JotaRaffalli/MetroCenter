525345593e0331796993fa74bf9edc5c7531ff1d
Reynaldo Quintero <reynaldoquintero96@hotmail.com>
2016-11-08 22:03:17 -0300

    Locales funcionando
    Esta hecho estático a través del url. Lo único que le falta es el view
    información, porque hasta ahora solo pone el nombre de título

--- api/controllers/LocalesController.js
+++ api/controllers/LocalesController.js
@@ -6,18 +6,21 @@
  */
 
 module.exports = {
-	'request': function(req, res){
-
-		res.redirect('/locales/informacion')
-	},
 
 	'informacion': function(req, res, next){
-		Locales.findOne({id: 1}, function foundLocales(err, local){
+		Locales.findOne({id: req.param('id')}, function foundLocales(err, local){
 
 			if(err) return next(err);
 			if(!local) return next();
-			res.view({
-				local: local
+			res.view('informacion', {
+				lcl: {
+		          id: local.id,
+		          nombre: local.nombre,
+		          menu: local.menu,
+		          horario: local.horario,
+		          pagoCredito: local.pagoCredito,
+		          pagoDebtio: local.pagoDebtio
+		        }
 			});
 		});
 	},
--- assets/js/public/locales/ModuloLocales.js
+++
@@ -1,1 +0,0 @@
-angular.module('ModuloLocales', ['toastr', 'compareTo']);
