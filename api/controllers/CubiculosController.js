f7de9c4d983fa5abbb5521e017f58e755de7b638
José Luis Raffalli <raffallijoseluis@gmail.com>
2016-11-11 21:38:26 -0400

    Api de Cubiculos

---
+++ api/controllers/CubiculosController.js
@@ -0,0 +1,11 @@
+/**
+ * CubiculosController
+ *
+ * @description :: Server-side logic for managing cubiculos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Cubiculos.js
@@ -0,0 +1,23 @@
+/**
+ * Cubiculos.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  	disponibilidad : { 
+  		type: 'boolean',
+  		required: true,
+  	},
+
+  	hora_salida : {
+  		type: 'time'
+  	}
+
+  }
+};
+
